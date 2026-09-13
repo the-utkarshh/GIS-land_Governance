5f5f98b521ff171d6090b7b95c0c2c7869cfe5b2
the-utkarshh <thakurprem1733@gmail.com>
2026-09-13 23:53:16 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://gis-bhunexus-frontend.onrender.com",
         "http://127.0.0.1:5500",
         "http://localhost:5500"
     ],
